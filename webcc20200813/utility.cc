acec9bcb9c3a3d4483f9fd3cda07ad6c5350e9d5
kagula086 <lee353086@msn.com>
2020-08-13 16:32:53 +0800

    new project.

---
+++ webcc20200813/utility.cc
@@ -0,0 +1,93 @@
+#include "webcc/utility.h"
+
+#include <algorithm>
+#include <ctime>
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include <iomanip>  // for put_time
+#include <iostream>
+#include <sstream>
+
+#include "webcc/string.h"
+#include "webcc/version.h"
+
+namespace webcc {
+namespace utility {
+
+const std::string& UserAgent() {
+  static auto s_user_agent = std::string("Webcc/") + WEBCC_VERSION;
+  return s_user_agent;
+}
+
+std::string GetTimestamp() {
+  std::time_t t = std::time(nullptr);
+  std::stringstream ss;
+  ss << std::put_time(std::gmtime(&t), "%a, %d %b %Y %H:%M:%S") << " GMT";
+  return ss.str();
+}
+
+std::size_t TellSize(const boost::filesystem::path& path) {
+  // Flag "ate": seek to the end of stream immediately after open.
+  std::ifstream stream{ path.c_str(), std::ios::binary | std::ios::ate };
+  if (stream.fail()) {
+    return kInvalidLength;
+  }
+  return static_cast<std::size_t>(stream.tellg());
+}
+
+bool ReadFile(const boost::filesystem::path& path, std::string* output) {
+  // Flag "ate": seek to the end of stream immediately after open.
+  std::ifstream stream{ path.c_str(), std::ios::binary | std::ios::ate };
+  if (stream.fail()) {
+    return false;
+  }
+
+  auto size = stream.tellg();
+  output->resize(static_cast<std::size_t>(size), '\0');
+  stream.seekg(std::ios::beg);
+  stream.read(&(*output)[0], size);
+  if (stream.fail()) {
+    return false;
+  }
+  return true;
+}
+
+void DumpByLine(const std::string& data, std::ostream& os,
+                const std::string& prefix) {
+  std::vector<std::string> lines;
+  split(lines, data, '\n');
+
+  std::size_t size = 0;
+
+  for (const std::string& line : lines) {
+    os << prefix;
+
+    if (line.size() + size > kMaxDumpSize) {
+      os.write(line.c_str(), kMaxDumpSize - size);
+      os << "..." << std::endl;
+      break;
+    } else {
+      os << line << std::endl;
+      size += line.size();
+    }
+  }
+}
+
+void PrintEndpoint(std::ostream& ostream,
+                   const boost::asio::ip::tcp::endpoint& endpoint) {
+  ostream << endpoint;
+  if (endpoint.protocol() == boost::asio::ip::tcp::v4()) {
+    ostream << ", v4";
+  } else if (endpoint.protocol() == boost::asio::ip::tcp::v6()) {
+    ostream << ", v6";
+  }
+}
+
+std::string EndpointToString(const boost::asio::ip::tcp::endpoint& endpoint) {
+  std::stringstream ss;
+  PrintEndpoint(ss, endpoint);
+  return ss.str();
+}
+
+}  // namespace utility
+}  // namespace webcc
---
+++ webccDemo/webccDemo.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+#include "../webcc20200813/webcc/logger.h"
+#include "../webcc20200813/webcc/response_builder.h"
+#include "../webcc20200813/webcc/server.h"
+
+
+
+class HelloView : public webcc::View {
+public:
+    webcc::ResponsePtr Handle(webcc::RequestPtr request) override {
+        if (request->method() == "GET") {
+            return webcc::ResponseBuilder{}.OK().Body("Hello, World!   My name is WebCC!")();
+        }
+
+        return {};
+    }
+};
+
+class LoginView : public webcc::View {
+public:
+    webcc::ResponsePtr Handle(webcc::RequestPtr request) override {
+        if (request->method() == "POST") {
+            return Post(request);
+        }
+
+        return {};
+    }
+
+private:
+    webcc::ResponsePtr Post(webcc::RequestPtr request) {
+        webcc::ResponsePtr rp;
+
+        if ((request->kagulaArgs_.find("username") != request->kagulaArgs_.end()) &&
+            (request->kagulaArgs_.find("password") != request->kagulaArgs_.end()))
+        {
+            //要把老的console給關閉, 否則一有東西輸出到std::cout綫程就會阻塞!
+            std::cout << "username:" << request->kagulaArgs_["username"].c_str() << std::endl;
+            std::cout << "password:" << request->kagulaArgs_["password"].c_str() << std::endl;
+            rp = webcc::ResponseBuilder{}.Json().Body("{\"ok\":1, \"message\":\"success\"}")();
+        }
+        else
+        {
+            rp = webcc::ResponseBuilder{}.Json().Body("{\"ok\":0, \"message\":\"none exist username or password!\"}")();
+        }
+
+
+        webcc::Header header("Access-Control-Allow-Origin", "*");//  这里最好明确的写出允许的域名;
+        rp->SetHeader(std::move(header));
+        return rp;
+    }
+};
+
+int main() {
+    try {
+        webcc::Server server(8080);
+
+        //server.Route("/", std::make_shared<HelloView>());
+        server.Route("/webccDemo/login.do", std::make_shared<LoginView>(), { "POST", "GET" });
+        server.Run();
+
+    }
+    catch (const std::exception&) {
+        return 1;
+    }
+
+    return 0;
+}
